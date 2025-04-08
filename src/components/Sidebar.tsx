24897f2b4579cc05390552cce146775c74a1fa5f
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2025-04-08 10:24:12 +0530

    Project Completed

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -9,7 +9,22 @@ const Sidebar = () => {
       <div className="flex-1 overflow-y-auto">
         <div className="p-4">
           <h3 className="text-lg font-semibold flex items-center">Lists</h3>
-          <button className="flex justify-center items-center mt-[1rem]">
+          <ul>
+            {lists.map((list, index) => (
+              <li
+                key={index}
+                className="p-2 hover:bg-[#ccc] rounded-lg
+                 cursor-pointer flex items-center"
+              >
+                <span className="mr-2">{list.emoji}</span>
+                {list.name}
+              </li>
+            ))}
+          </ul>
+          <button
+            onClick={openListModal}
+            className="flex justify-center items-center mt-[1rem]"
+          >
             <FaPlus className="mr-2" /> List
           </button>
         </div>
@@ -17,8 +32,23 @@ const Sidebar = () => {
           <h3 className="text-lg font-semibold flex items-center">
             Workspaces
           </h3>
-          <button className="flex justify-center items-center mt-[1rem]">
-            <FaPlus className="mr-2" /> Workspaces
+          <ul>
+            {workspaces.map((workspace, index) => (
+              <li
+                key={index}
+                className="p-2 hover:bg-[#ccc] rounded-lg 
+                cursor-pointer flex items-center"
+              >
+                <span className="mr-2">{workspace.emoji}</span>
+                {workspace.name}
+              </li>
+            ))}
+          </ul>
+          <button
+            onClick={openWorkspaceModal}
+            className="flex justify-center items-center mt-[1rem]"
+          >
+            <FaPlus className="mr-2" /> Workspace
           </button>
         </div>
       </div>
