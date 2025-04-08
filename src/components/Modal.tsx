e18704cbba8e709f88f576ba58c0d95e3945cc85
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2025-04-08 10:16:21 +0530

    Modal Component Completed

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -30,7 +30,10 @@ const Modal = () => {
   if (!isListModalOpen && !isWorkspaceModalOpen) return null;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50 z-50">
+    <div
+      className="fixed inset-0 flex items-center
+     justify-center bg-gray-900 bg-opacity-50 z-50"
+    >
       <div className="bg-white p-6 rounded-lg w-80">
         <div className="flex justify-between items-center mb-4">
           <h2 className="text-xl font-bold">{`Create New ${modalType}`}</h2>
@@ -55,6 +58,12 @@ const Modal = () => {
           placeholder="Enter emoji (optional)"
           className="border border-gray-300 p-2 rounded-lg w-full mb-4"
         />
+        <button
+          onClick={handleSave}
+          className="bg-black text-white px-4 py-2 rounded-lg"
+        >
+          Save
+        </button>
       </div>
     </div>
   );
